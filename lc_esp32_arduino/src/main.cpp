2ee163cddbc404754694174c6abe025de1444704
dabramov <dzmitry.abramau@gmail.com>
2021-06-12 01:33:37 +0300

    print build number and date

--- lc_esp32_arduino/src/main.cpp
+++ lc_esp32_arduino/src/main.cpp
@@ -1,6 +1,24 @@
 #include <Arduino.h>
 
+#include "version.h"
+
 void setup() {
+  Serial.begin(4000000);
+  Serial.setDebugOutput(true);
+  printf("\n\n");
+  printf("========================\n");
+  if (BUILD_NUMBER < 0)
+  {
+    printf("  LOCAL BUILD\n");
+  }
+  else
+  {
+    printf("  BUILD NUMBER: %05d\n", BUILD_NUMBER);
+  }
+  printf("  %s %s\n", __DATE__, __TIME__);
+  printf("========================\n");
+  printf("\n");
+
   // put your setup code here, to run once:
 }
 
