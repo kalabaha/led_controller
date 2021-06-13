b19fc2be3de0d48249fae50ed8df124c61b6fac0
dabramov <dzmitry.abramau@gmail.com>
2021-06-13 22:25:09 +0300

    add config.h

---
+++ lc_esp32_arduino/src/config.h
@@ -0,0 +1,3 @@
+#pragma once
+
+#define CONFIG_UART_BAUD_RATE           (4000000)
--- lc_esp32_arduino/src/main.cpp
+++ lc_esp32_arduino/src/main.cpp
@@ -1,27 +1,30 @@
 #include <Arduino.h>
 
 #include "version.h"
+#include "config.h"
 
-void setup() {
-  Serial.begin(4000000);
-  Serial.setDebugOutput(true);
-  printf("\n\n");
-  printf("========================\n");
-  if (BUILD_NUMBER < 0)
-  {
-    printf("  LOCAL BUILD\n");
-  }
-  else
-  {
-    printf("  BUILD NUMBER: %05d\n", BUILD_NUMBER);
-  }
-  printf("  %s %s\n", __DATE__, __TIME__);
-  printf("========================\n");
-  printf("\n");
+void setup()
+{
+    Serial.begin(CONFIG_UART_BAUD_RATE);
+    Serial.setDebugOutput(true);
+    printf("\n\n");
+    printf("========================\n");
+    if (BUILD_NUMBER < 0)
+    {
+        printf("  LOCAL BUILD: %05d\n", -BUILD_NUMBER);
+    }
+    else
+    {
+        printf("  BUILD NUMBER: %05d\n", BUILD_NUMBER);
+    }
+    printf("  %s %s\n", __DATE__, __TIME__);
+    printf("========================\n");
+    printf("\n");
 
-  // put your setup code here, to run once:
+    // put your setup code here, to run once:
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+    // put your main code here, to run repeatedly:
 }
